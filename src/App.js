baaa54b0bec5d04dcc5abaacff5ee4b33c4064eb
codermonkvipin <codermonkvipin@gmail.com>
2023-09-07 10:25:14 +0530

    created login and sign up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-
-
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-5xl font-bold text-green-800">
-      Namaste Everyone, let's build Netflix GPT
-    </div>
-  );
+  
+  return <Body />;
 }
 
 export default App;
