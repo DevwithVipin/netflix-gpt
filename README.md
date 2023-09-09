d68ab975bffccc695fdabeee86c916d25cf381d5
codermonkvipin <codermonkvipin@gmail.com>
2023-09-09 11:52:40 +0530

    Bug fixes

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Netflix GPT
 
-- Create React App
-- Configured TailwindCSS 
-- 
+Create React App
+Configured TailwindCSS
+Header
+Routing of App
+Login Form
+Sign up Form
+Form Validation
+useRef Hook
+Firebase Setup
+Deploying our app to production
+Create SignUp User Account
+Implement Sign In user Api
+Created Redux Store with userSlice
+Implemented Sign out
+Update Profile
+BugFix: Sign up user displayName and profile picture update
+BugFix: if the user is not logged in Redirect /browse to Login Page and vice-versa
+Unsubscibed to the onAuthStateChanged callback
 
 
 # Features
--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,7 @@ import {
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { USER_AVATAR, BG_URL } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -34,7 +35,7 @@ const Login = () => {
           const user = userCredential.user;
           updateProfile(user, {
             displayName: name.current.value,
-            photoURL: "https://avatars.githubusercontent.com/u/12824231?v=4",  
+            photoURL: USER_AVATAR,  
           })
             .then(() => {
               navigate("/browse");
@@ -73,7 +74,7 @@ const Login = () => {
       <Header />
       <div className="absolute">
         <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          src={BG_URL}
           alt="logo"
         />
       </div>
