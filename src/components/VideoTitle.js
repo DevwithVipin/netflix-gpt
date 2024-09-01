b57ad04cffa0863d22acded7916b54646ad0f85f
Vipin Kumar <114944948+DevwithVipin@users.noreply.github.com>
2024-09-01 22:48:17 +0530

    Update VideoTitle.js

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,11 +1,11 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="aspect-video w-full pt-[20%] px-4 lg:py-34 sm:mt-4 sm:py-8 md:py-10 lg:py-32 absolute text-white bg-gradient-to-r from-black flex flex-col justify-center items-start">
-      <h1 className="text-2xl md:text-2xl md:mt-12 font-bold">{title}</h1>
-      <p className="flex hidden sm:visible text-2xl w-1/4 lg:w-1/4 md:flex md:flex-wrap md:text-xs sm:text-xs md:m-1">
+    <div className="aspect-video w-full pt-[20%] p-10 lg:py-34 sm:mt-4 sm:py-8 md:py-10 lg:py-32 absolute text-white bg-gradient-to-r from-black flex flex-col justify-center items-start">
+      <h1 className="text-3xl md:text-2xl md:mt-12 pb-3 font-bold">{title}</h1>
+      <p className="flex cs:invisible  sm:visible text-xs w-1/4  lg:w-1/4 md:flex md:flex-wrap md:text-xs sm:text-xs md:m-5">
         {overview}
       </p>
-      <div className="space-between lg:py-34 md:m-0 flex">
+      <div className="justify-between gap-5  lg:py-34 md:m-0 flex pt-4 w-[15rem]">
         <button className="bg-white text-black md:inline-block md:mt-2 p-4 px-12 text-xl bg-opacity-500 rounded-lg">
           ▶️ Play
         </button>
