7864cfa4b182bc33b61668e20a4b8281faca63ea
Vipin Kumar <114944948+DevwithVipin@users.noreply.github.com>
2024-09-01 22:47:57 +0530

    Update SecondaryContainer.js

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -3,15 +3,15 @@ import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
- 
+
 
   return (
     movies.nowPlayingMovies && (
-      <div className="bg-black">
+      <div className="bg-black pl-10 ">
         <div className="  xl:-mt-52 sm:text-xs sm:-mt-2 md:text-xs lg:-mt-32  md:-mt-42 md:pl-4 md:relative ">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Top Rated Movies"} movies={movies.topRatedMovies} />
-      
+
           <MovieList
             title={"Upcoming Movies"}
             movies={movies.upcomingMovies}
